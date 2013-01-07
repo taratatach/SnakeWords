3986d5f2631df551d58371e04da7aea686ef2ef2
taratatach <erwanguyader@gmail.com>
2013-01-07 14:48:43 +0100

    fichiers RoR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnakeWords::Application.config.secret_token = '1d287bb0271484935fde1f90208c6d52645a7af5759ec952b9df43bcb982fc5389066a151a2e48bc65982c4df62032d2b5c1e1279a0e79e23e8aa57107e280f7'
