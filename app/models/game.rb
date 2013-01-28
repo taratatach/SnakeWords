b56d7ea212bf6d78602dd6b04d9b9d22773fcc1e
taratatach <erwanguyader@gmail.com>
2013-01-28 14:41:23 +0100

    validates are back on Player and Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ActiveRecord::Base
+  validates :size, :presence => true, :numericality => true
+  
   attr_accessible :dictionary, :finished, :size
 
   has_and_belongs_to_many :players
